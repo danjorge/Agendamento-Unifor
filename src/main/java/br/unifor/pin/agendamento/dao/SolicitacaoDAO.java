7ac514739f419ed2176d4253f79e253cb8a6ed47
Daniel Jorge de Souza <danjorge@gmail.com>
2015-11-25 16:14:54 -0300

    alteração na pesquisa de solicitação e css de templates

--- src/main/java/br/unifor/pin/agendamento/dao/SolicitacaoDAO.java
+++ src/main/java/br/unifor/pin/agendamento/dao/SolicitacaoDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.unifor.pin.agendamento.entity.Solicitacao;
 import br.unifor.pin.agendamento.entity.Status;
 import br.unifor.pin.agendamento.entity.Usuarios;
+import br.unifor.pin.agendamento.managers.MenuManagedBean;
 
 
 @Repository
@@ -21,6 +23,9 @@ public class SolicitacaoDAO {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+	
+	@Autowired
+	private MenuManagedBean menuManagedBean;
 	
 	
 	public List<Solicitacao> retornaListaSolicitacoesPorCurso(Usuarios usuario){
@@ -72,11 +77,13 @@ public class SolicitacaoDAO {
 		return (List<Solicitacao>) entityManager.createQuery("Select s "
 														   + "from Solicitacao s "
 														   + "inner join fetch s.usuario u "
+														   + "inner join fetch u.cursos c "
 														   + "inner join fetch s.statusSolicitacao st "
 														   + "where "
 														   + (sol.getId() != null ? "s.id = " + sol.getId() : "")
 														   + (sol.getId() != null ? "and upper(s.assunto) like upper('%' || :assunto || '%') " : "upper(s.assunto) like upper('%' || :assunto || '%') ") 
 														   + "and :statusSolicitacaoIdAux is NULL or s.statusSolicitacao.id = :statusSolicitacaoId "
+														   + "and c.id = " +menuManagedBean.getSeguranca().getUsuario().getCursos().get(0).getId() + " "
 														   + "order by s.id")
 														   .setParameter("assunto", sol.getAssunto())
 														   .setParameter("statusSolicitacaoIdAux", sol.getStatusSolicitacao().getId())
