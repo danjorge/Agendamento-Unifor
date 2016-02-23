9589f37dffe69ef50ef16698c7a916472db118c0
Daniel Jorge de Souza <danjorge@gmail.com>
2016-02-23 11:55:48 -0300

    Atualização entidade papeis

--- src/main/java/br/unifor/pin/agendamento/entity/Papeis.java
+++ src/main/java/br/unifor/pin/agendamento/entity/Papeis.java
@@ -6,9 +6,9 @@ package br.unifor.pin.agendamento.entity;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.Entity;
 //github.com/danjorge/Agendamento-Unifor
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,7 +16,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import br.unifor.pin.agendamento.utils.BaseEntity;
